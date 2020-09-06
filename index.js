72e5d7c36183671254f3105aa4c632f89daf4061
Rich Wilson <richard.wilson@smgdesign.org>
2020-09-07 00:59:39 +0100

    updated socket listener to push requeest listenert

---
+++ index.js
@@ -0,0 +1,152 @@
+const Firestore = require('@google-cloud/firestore');
+const admin = require('firebase-admin');
+const projectId = 'stoked-reality-284921';
+
+const publish = (
+  topicName = 'ex-gateway',
+  source = 'app-engine',
+  data = {}
+) => {
+  const {PubSub} = require('@google-cloud/pubsub');
+  // Instantiates a client
+  const pubsub = new PubSub({projectId});
+
+  async function publishMessage() {
+    const dataBuffer = Buffer.from(JSON.stringify(data));
+
+    const messageId = await pubsub.topic(`${topicName}-${source}`).publish(dataBuffer);
+    return messageId;
+  }
+
+  return publishMessage();
+};
+
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
+exports.manage = async (event, context, callback) => {
+  const message = event && event.data ? JSON.parse(Buffer.from(event.data, 'base64').toString()) : null;
+  if (message === null) {
+    callback();
+  }
+  const {domain, action, command, socketId, payload, user, source} = message;
+  const db = new Firestore({
+    projectId,
+  });
+  if (message.payload.start_date) {
+    message.payload.start_date = Firestore.Timestamp.fromDate(new Date(Date.parse(message.payload.start_date)));
+  }
+  if (message.payload.end_date) {
+    message.payload.end_date = Firestore.Timestamp.fromDate(new Date(Date.parse(message.payload.end_date)));
+  }
+  switch (command) {
+    case 'create':
+      try {
+        const docRef = db.collection('polls').doc();
+    
+        await docRef.set({
+          ...payload,
+          addedBy: user.id,
+          addedAt: Firestore.FieldValue.serverTimestamp()
+        });
+   
+        await Promise.all([
+          publish('ex-manage', { domain, action, command, payload: { ...payload, id: docRef.id }, user, socketId }),
+          publish('ex-gateway', source, { domain, action, command, payload: { ...payload, id: docRef.id }, user, socketId })
+        ]);
+        callback();
+      } catch (error) {
+        await publish('ex-gateway', source, { error: error.message, domain, action, command, payload, user, socketId });
+        callback(0);
+      }
+      break;
+    case 'update':
+      try {
+        const docRef = db.collection('polls').doc(payload.id);
+    
+        await docRef.set({
+          ...payload,
+          updatedBy: user.id,
+          updatedAt: Firestore.FieldValue.serverTimestamp()
+        }, {
+          merge: true
+        });
+    
+        await publish('ex-gateway', source, { domain, action, command, payload: { ...payload }, user, socketId });
+        callback();
+      } catch (error) {
+        await publish('ex-gateway', source, { error: error.message, domain, action, command, payload, user, socketId });
+        callback(0);
+      }
+      break;
+    case 'read':
+      try {
+        const docRef = db.collection('polls').doc(payload.id);
+    
+        const poll = await docRef.get();
+
+        if (!poll.exists) {
+          throw new Error('item not found');
+        }
+    
+        await publish('ex-gateway', source, { domain, action, command, payload: room.data(), user, socketId });
+        callback();
+      } catch (error) {
+        await publish('ex-gateway', source, { error: error.message, domain, action, command, payload, user, socketId });
+        callback(0);
+      }
+      break;
+    case 'get':
+      try {
+        const docRef = db.collection('polls').doc(payload.id);
+        const poll = await docRef.get();
+
+        if (!poll.exists) {
+          throw new Error('item not found');
+        }
+
+        let data = poll.data();
+
+        await publish('ex-gateway', source, { domain, action, command, payload: { id: payload.id, ...data }, user, socketId });
+        callback();
+      } catch (error) {
+        await publish('ex-gateway', source, { error: error.message, domain, action, command, payload, user, socketId });
+        callback(0);
+      }
+      break;
+    case 'answer':
+      // user answers an item
+      try {
+        let data = {};
+        if (payload.data.instance) {
+          const docRef = db.collection('polls').doc(payload.id);
+          const poll = await docRef.get();
+
+          if (!poll.exists) {
+            throw new Error('item not found');
+          }
+
+          let data = poll.data(payload);
+
+          const questionRef = docRef.collection('questions').doc(payload.data.question);
+          const answerRef = questionRef.collection('responses').doc(payload.data.answer);
+          await answerRef.set({
+            responses: firebase.firestore.FieldValue.increment(1),
+            respondants: admin.firestore.FieldValue.arrayUnion(user.id)
+          }, { merge: true });
+          const question = await questioneRef.get();
+          payload.data.question = question.data();
+        } else {
+          throw new Error('question and answer are required');
+        }
+        await publish('ex-gateway', source, { domain, action, command, payload: { ...payload }, user, socketId });
+        callback();
+      } catch (err) {
+        await publish('ex-gateway', source, { error: error.message, domain, action, command, payload, user, socketId });
+        callback(0);
+      }
+      break;
+};
